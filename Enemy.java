618d26ebf4a4034a8cda6c6f7816cb36db39bb33
Nathan <nathan.bhandari@ucalgary.ca>
2018-07-16 19:53:47 -0600

    fixed a logical error with attackLogic method

--- Enemy.java
+++ Enemy.java
@@ -1,8 +1,8 @@
 /**
-* weak attack = 15HP
-* Strong attack = 40HP but takes 2 turns to work
-* Parry has a 50% chance of not working otherwise reduces damage to 0
-* Potion recovers 25HP
+* moves[0] = weak attack = 15HP
+* moves [1] = Strong attack = 40HP but takes 2 turns to work
+* moves[2] = Parry has a 50% chance of not working otherwise reduces damage to 0
+* moves[3] = Potion recovers 25HP
 * engageBattle should have an enemy as a parameter to check specifically which enemy is 
 * being fought.
 */
@@ -10,7 +10,8 @@
 
 public class Enemy extends Avatar{
 private boolean key; // if true, enemy has a key. false if they do not have a key 
-
+private boolean hasPotion; // shows if enemy has any potions left 
+private Potion[] potions; // list of potions the enemy has 
 /**
 * Constructor for the class, likely to be used at game loop initialization
 * sets the initial health, defence, attack, and allowed moves for the enemy 
@@ -20,11 +21,13 @@ private boolean key; // if true, enemy has a key. false if they do not have a ke
 * @param attack - enemy attack value as an integer
 * @param moves - list of all possible enemy attacks
 * @param key - true if enemy has a key, false otherwise
+* @param key - true if enemy has a potion, false otherwise
 */
 
-public Enemy(int health, int defence, int attack, String[] moves, boolean key){
+public Enemy(int health, int defence, int attack, String[] moves, boolean key, boolean hasPotion){
 super(health, defence, attack, moves); // invokes Avatar constructor
 this.key = key;
+this.hasPotion = hasPotion;
 }
 
 /**
@@ -50,20 +53,21 @@ this.key = key;
 * Decides what attack the enemy will use. This is accomplished by deciding the most 
 * efficient way to reduce player health to 0 while keeping the enemy's health as high as 
 * possible.
-* @param e - enemy in battle 
-* @return e.moves[] - the attack the enemy decides to use
+* @return moves[] - the attack the enemy decides to use
 */
-public String attackLogic(Enemy e){
+
+
+public String attackLogic(){
 if(player.getHealth() <= 15){
-return e.moves[0];
-} else if(player.getHealth()<=40 && e.getHealth() > 30){
-return e.moves[1];
-} else if(e.getHealth() <= 25 && e.hasPotion()==true){
-return e.moves[3];
-} else if(e.getHealth()<=40 && e.hasPotion() ==false){
-return e.moves[2];
+return this.moves[0];
+} else if(player.getHealth()<=40 && this.getHealth() > 30){
+return this.moves[1];
+} else if(this.getHealth() <= 25 && this.hasPotion==true){
+return this.moves[3];
+} else if(this.getHealth()<=40 && this.hasPotion ==false){
+return this.moves[2];
 } else{
-return e.moves[0];
+return this.moves[0];
 }
 }
 
