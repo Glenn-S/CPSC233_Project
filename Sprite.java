5ae27410f81a6d0c7adfb32138bac95b1cc9ee4a
momoinori <40900037+momoinori@users.noreply.github.com>
2018-07-18 18:04:17 -0600

    Add files via upload

---
+++ Sprite.java
@@ -0,0 +1,105 @@
+import java.awt.Image;
+
+public class Sprite
+{
+	private Image SpriteImage;
+	Sprite s = new Sprite();
+	private String name;
+	private boolean exists = true;
+	private String[] dialogue;
+	private boolean overlapsWith = false;
+	private int xCoord;
+	private int ySizemax;
+	private int ySizemin;
+	private int ySize;
+	private int xSize;
+	private int xSizemax;
+	private int xSizemin;
+	private int yCoord;
+	
+	public Sprite() {
+		
+	}
+	
+	
+	public int getxCoord() {
+		return xCoord;
+	}
+
+	public void setxCoord(int xCoord) {
+		this.xCoord = xCoord;
+	}
+	public int getyCoord() {
+		return yCoord;
+	}
+	
+	public void setyCoord(int yCoord) {
+		this.yCoord = yCoord;
+	}
+	public int getxSize() {
+		return xSize;
+	}
+	public void setxSize(int xSize) {
+		this.xSizemax = this.xCoord + xSize;
+		this.xSizemin = this.xCoord - xSize;
+	}
+	public int getySize() {
+		return ySize;
+	}
+	public void setySize(int ySize) {
+		this.ySizemax = this.yCoord + ySize;
+		this.ySizemin = this.yCoord - ySize;
+	}
+
+	public void setName(String setname){
+		this.name = setname;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setSpriteImage(){
+		
+	}
+	public Image getSpriteImage(){
+		return SpriteImage;
+	}
+	
+	public void setExists(boolean exist){
+		this.exists  = exist;		
+	}
+	public boolean getExists(){
+		return exists;
+	}
+
+	public void setDialogue(String dialog){
+		dialogue = dialog;
+	}
+	
+	public String getDialogue(){
+		return dialogue;
+	}
+	
+	public void setoverlapsWith(player player){
+		if (player.xCoord <= Enemy.xCoordmax && player.xCoord >= Enemy.xCoordmin && player.yCoord <= Enemy.yCoordmax && player.yCoord >= Enemy.yCoordmin){
+			overlapsWith = true;
+		}
+		else if(player.xCoord == Terrain.xCoord && player.yCoord == Terrain.yCoord ) {
+			overlapsWith = true;
+		}
+		else if(player.xCoord == Edges.xCoord && player.yCoord == Edges.yCoord) {
+			overlapsWith = true;
+		}
+		else if (player.Location == Iteam.Location && player.yCoord == Iteam.yCoord ) {
+			overlapsWith = true;
+		}
+		overlapsWith = false;
+		}
+		
+	}
+	
+	public boolean getoverlapsWith(){
+		return overlapsWith;
+	}		
+}
