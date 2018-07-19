3b66c527b58472577234c86566cee345d78ac25c
Glenn <skeltong2@gmail.com>
2018-07-18 23:24:22 -0600

    commit to merge

--- Sprite.java
+++ Sprite.java
@@ -8,67 +8,23 @@ public class Sprite
 	private boolean exists = true;
 	private String[] dialogue;
 	private boolean overlapsWith = false;
-<<<<<<< HEAD
-	private int xCoord;
-	private int ySizemax;
-	private int ySizemin;
-	private int ySize;
-	private int xSize;
-	private int xSizemax;
-	private int xSizemin;
-	private int yCoord;
-
-=======
 	private Location Coord;
-	
-	
+
+
 	public Sprite(Location coord,String name,Image SpriteImage,boolean Exists,String[] Dialogue,boolean overlapsWith) {
-		
+
 	}
->>>>>>> master
 	public Sprite() {
 
 	}
-<<<<<<< HEAD
 
-
-	public int getxCoord() {
-		return xCoord;
-	}
-
-	public void setxCoord(int xCoord) {
-		this.xCoord = xCoord;
-	}
-	public int getyCoord() {
-		return yCoord;
-	}
-
-	public void setyCoord(int yCoord) {
-		this.yCoord = yCoord;
-	}
-	public int getxSize() {
-		return xSize;
-	}
-	public void setxSize(int xSize) {
-		this.xSizemax = this.xCoord + xSize;
-		this.xSizemin = this.xCoord - xSize;
-	}
-	public int getySize() {
-		return ySize;
-	}
-	public void setySize(int ySize) {
-		this.ySizemax = this.yCoord + ySize;
-		this.ySizemin = this.yCoord - ySize;
-=======
-	
 	public void setCoord(int xcoord,int ycoord) {
 		int xCoord = xcoord;
 		int yCoord = ycoord;
 	}
-	
+
 	public Location getCoord() {
 		return Coord;
->>>>>>> master
 	}
 
 	public void setName(String setname){
@@ -115,15 +71,9 @@ public class Sprite
 			overlapsWith = true;
 		}
 		overlapsWith = false;
-<<<<<<< HEAD
-	}
-
-
-=======
 		}
-		
-	
->>>>>>> master
+
+
 	public boolean getoverlapsWith(){
 		return overlapsWith;
 	}
