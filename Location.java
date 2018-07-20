90aa89f694a6ef9e37f95c3f5cf08f21797cfcdb
momoinori <40900037+momoinori@users.noreply.github.com>
2018-07-19 20:10:07 -0600

    Add files via upload

--- Location.java
+++ Location.java
@@ -1,40 +1,41 @@
 
-public class Location {
+/*
+ * Instance variables
+ */
+public class Location {	
 	private int xCoord;
 	private int yCoord;
 	private int xSize;
-	private int xSizemax;
-	private int xSizemin;
 	private int ySize;
-	private int ySizemax;
-	private int ySizemin;
-
-	public int getxCoord() {
+	
+	public int getxCoord() { 				//getter for xCoord
 		return xCoord;
 	}
 
-	public void setxCoord(int xCoord) {
-		this.xCoord = xCoord;
+	public void setxCoord(int xcoord) {    //setter for xCoord
+		if (xcoord <= 200 && xcoord >= 0) {
+		this.xCoord = xcoord;
+		}
 	}
-	public int getyCoord() {
+	public int getyCoord() {				//getter for yCoord
 		return yCoord;
 	}
-
-	public void setyCoord(int yCoord) {
-		this.yCoord = yCoord;
+	
+	public void setyCoord(int ycoord) {    //setter for yCoord
+		if (ycoord >= 0 && ycoord <= 40) {
+			this.yCoord = yCoord;
+		}
 	}
-	public int getxSize() {
+	public int getxSize() {	//getter for xSize
 		return xSize;
 	}
-	public void setxSize(int xSize) {
-		this.xSizemax = this.xCoord + xSize;
-		this.xSizemin = this.xCoord - xSize;
+	public void setxSize(int xsize) {		//setter for xSize
+		this.xSize = xsize;
 	}
-	public int getySize() {
+	public int getySize() {					//getter for ySize
 		return ySize;
 	}
-	public void setySize(int ySize) {
-		this.ySizemax = this.yCoord + ySize;
-		this.ySizemin = this.yCoord - ySize;
+	public void setySize(int ysize) {		//setter for ySize
+		this.ySize = ysize;
 	}
 }
--- Sprite.java
+++ Sprite.java
@@ -1,89 +1,85 @@
 import java.awt.Image;
-
+//it's the parent class of  Avator , Potion ,Weapon and Defence
 public class Sprite
-{
+{	//instance variables
 	private Image spriteImage;
 	private String name;
 	private boolean exists = true;
 	private String[] dialogue;
 	private boolean overlapsWith = false;
 	private Location coord;
-<<<<<<< HEAD
-
-=======
+	Sprite s = new Sprite();
 	
->>>>>>> master
 	//constructors
-	public Sprite(Location coord,String name,Image spriteImage,boolean exists,String[] dialogue,boolean overlapsWith) {
-		this.spriteImage = spriteImage;
+	public Sprite(Location coord,String name,Image SpriteImage,boolean Exists,String[] Dialogue,boolean overlapsWith) {
 		this.name = name;
-		this.exists = exists;
-		this.dialogue = dialogue;
+		this.coord= coord;
+		this.spriteImage = SpriteImage;
+		this.exists = Exists;
+		this.dialogue = Dialogue;
 		this.overlapsWith = overlapsWith;
-		this.coord = coord;
 	}
 	public Sprite() {
-
+		
 	}
-
+	
 	public void setCoord(int xcoord,int ycoord) { //set xCoord and yCoord
 		int xCoord = xcoord;
 		int yCoord = ycoord;
 	}
-
+	
 	public Location getCoord() {				//set xCoord and yCoord as a type Location
 		return coord;
 	}
 
-	public void setName(String setname){
+	public void setName(String setname){		//set the name
 		this.name = setname;
 	}
-
+	
 	public String getName(){					//get the name
 		return name;
 	}
-
+	
 	public void setSpriteImage(){				//set the image
-
+		
 	}
-	public Image getSpriteImage(){				//get the image
+	public Image getSpriteImage(){				//get the image 
 		return spriteImage;
 	}
-
+	
 	public void setExists(boolean exist){		//set the exist state of iteams and enemies.
-		this.exists  = exist;
+		this.exists  = exist;		
 	}
-	public boolean getExists(){
+	public boolean getExists(){					//get the exist state
 		return exists;
 	}
 
-	public void setDialogue(String dialog){
+	public void setDialogue(String[] dialog){	//set the dialogue
 		dialogue = dialog;
 	}
-
+	
 	public String[] getDialogue(){				//get the dialogue
 		return dialogue;
 	}
-
+	
 	public void setoverlapsWith(player player){//set if player is overlapswith enemy , terrain ,potions or edges
 		if (player.xCoord <= Enemy.xCoor + Enemy.xSize && player.xCoord >= Enemy.xCoor - Enemy.xSize && player.yCoord <= Enemy.yCoord + Enemy.ySize && player.yCoord >= Enemy.yCoord - Enemy.ySize){
 			overlapsWith = true;//it returns true if the player is in an area of rectangle with x range [xcood - xsize, xccord + xsize] and y range [ycoord - ysize , ycoord + ysize]
 		}
 		else if(player.xCoord == Terrain.xCoord && player.yCoord == Terrain.yCoord ) {
-			overlapsWith = true;
+			overlapsWith = true;//it returns true if the player is ate the same position of a terrain
 		}
 		else if(player.xCoord == Edges.xCoord && player.yCoord == Edges.yCoord) {
-			overlapsWith = true;
+			overlapsWith = true;//it returns true if the player is ate the same position of the edge
 		}
 		else if (player.Location == Iteam.Location && player.yCoord == Iteam.yCoord ) {
-			overlapsWith = true;
+			overlapsWith = true;//it returns true if the player is ate the same position of an iteam
 		}
 		overlapsWith = false;
-
 		}//it returns false if none of above is true
-
-
+		
+	
 	public boolean getoverlapsWith(){	//get the overlapsWith
 		return overlapsWith;
-	}
+	}		
 }
