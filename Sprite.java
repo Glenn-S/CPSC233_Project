2de6928261799539ad347fa2272f893cfd398ef0
Glenn <skeltong2@gmail.com>
2018-07-19 22:19:41 -0600

    fixed changes lost in Sprite

--- Sprite.java
+++ Sprite.java
@@ -1,31 +1,37 @@
 import java.awt.Image;
-public class Sprite
-{	
-/*it's the parent class of  Avator , Potion ,Weapon and Defence
- * 
+/**
+ * it's the parent class of  Avator , Potion ,Weapon and Defence
+ *
  * @version 		20.39   19  July 2018
  * @author    		Yuhui  Yan
- * 
  *
-	 *instance variable
-	 * @param spriteImage   image that represent an object on map as a type Image
-	 * @param name  		the name of the player/avatar/iteams  as a type String
-	 * @param exist			it's true if the objecet is exit on the map  as a type boolean
-	 * @param dialogue		the dialogue between avatars    as a type of string array
-	 * @param overlapsWith  it's true if the player is overlaps with an enemy,a iteam , edge or terrains as a type boolean
-	 * @param coord			the location includes xCoordinate and yCoordinate  as a type of Location
-	 * @param s				it's an object of Sprite      as a type of Sprite
- 	*/
-	private Image spriteImage;
-	private String name;
-	private boolean exists = true;
-	private String[] dialogue;
-	private boolean overlapsWith = false;
-	private Location coord;
-	Sprite s = new Sprite();
-	
+ *
+ *instance variable
+ * spriteImage   image that represent an object on map as a type Image
+ * name  		the name of the player/avatar/iteams  as a type String
+ * exist			it's true if the objecet is exit on the map  as a type boolean
+ * dialogue		the dialogue between avatars    as a type of string array
+ * overlapsWith  it's true if the player is overlaps with an enemy,a iteam , edge or terrains as a type boolean
+ * coord			the location includes xCoordinate and yCoordinate  as a type of Location
+ * s				it's an object of Sprite      as a type of Sprite
+ */
+public class Sprite {
+	protected String title;
+    protected Location coordinate;
+    protected Image spriteImage;
+    protected char spriteSymbol; // will become the sprite image class later
+    protected String[] spriteMessages;
+    protected boolean existence;
+    protected boolean overlapsWith;
+
 	//constructors
-	public Sprite(Location coord,String name,Image SpriteImage,boolean Exists,String[] Dialogue,boolean overlapsWith) {
+	public Sprite(String title,
+                  Location coordinate,
+                  Image spriteImage,
+                  char spriteSymbol, // will become the sprite image class later
+                  String[] spriteMessages,
+                  boolean existence,
+                  boolean overlapsWith) {
 		this.name = name;
 		this.coord= coord;
 		this.spriteImage = SpriteImage;
@@ -33,80 +39,90 @@ public class Sprite
 		this.dialogue = Dialogue;
 		this.overlapsWith = overlapsWith;
 	}
+
 	public Sprite() {
-		
+		this.title = "";
+	    this.coordinate = null;
+	    this.spriteImage = null;
+	    this.spriteSymbol = ' '; // will become the sprite image class later
+	    this.spriteMessages = null;
+	    this.existence = true; 
+	    this.overlapsWith = false;
 	}
-	/*
-	 * @param xcoord     the xCoordinate of the sprite as a type of integer
-	 * @param ycoord     the yCoordinate of the Sprite as a type of integer
-	 */	
+
+	/**
+	 * @param xcoord the xCoordinate of the sprite as a type of integer
+	 * @param ycoord the yCoordinate of the Sprite as a type of integer
+	 */
 	public void setCoord(int xcoord,int ycoord) { //set xCoord and yCoord
 		int xCoord = xcoord;
 		int yCoord = ycoord;
 	}
-	
-	/*
+
+	/**
 	 * @return coord 	it's the Location of the sprite as a type of integer
 	 */
 	public Location getCoord() {				//set xCoord and yCoord as a type Location
 		return coord;
 	}
-	
-	/*
-	 * @param setname   the name of the sprite  as a type of String 
-	 */	
+
+	/**
+	 * @param setname   the name of the sprite  as a type of String
+	 */
 	public void setName(String setname){		//set the name
 		this.name = setname;
 	}
-	
-	/*
-	 * @return name   the name of the sprite  as a type of String 
-	 */	
+
+	/**
+	 * @return name   the name of the sprite  as a type of String
+	 */
 	public String getName(){					//get the name
 		return name;
 	}
-	
-	
+
+
 	public void setSpriteImage(){				//set the image
-		
+
 	}
-	
-	/*
-	 * @return spriteImage      renew the spriteImage as a type of Image
-	 */	
-	public Image getSpriteImage(){				//get the image 
+
+	/**
+	 * @return spriteImage - renew the spriteImage as a type of Image
+	 */
+	public Image getSpriteImage(){				//get the image
 		return spriteImage;
 	}
-	
-	/*
-	 * @param exist           the sate of iteams and enemis if they are exist    as a type of boolean
-	 */	
+
+	/**
+	 * @param exist the sate of iteams and enemis if they are exist
+	 * as a type of boolean
+	 */
 	public void setExists(boolean exist){		//set the exist state of iteams and enemies.
-		this.exists  = exist;		
+		this.exists  = exist;
 	}
-	
-	/*
-	 * @return exists  		  renew the exist state of iteams and enemies   as a type of boolean 
+
+	/**
+	 * @return exists - renew the exist state of iteams and enemies
+	 * as a type of boolean
 	 */
 	public boolean getExists(){					//get the exist state
 		return exists;
 	}
-	/*
-	 * @param dialog 			set dialogues as a type of String array
-	 */	
+	/**
+	 * @param dialog set dialogues as a type of String array
+	 */
 	public void setDialogue(String[] dialog){	//set the dialogue
 		dialogue = dialog;
 	}
-	/*
-	 * @return dialogue			renew the dialogue we need as a String array
+	/**
+	 * @return dialogue	- renew the dialogue we need as a String array
 	 */
 	public String[] getDialogue(){				//get the dialogue
 		return dialogue;
 	}
-	
-	
-	/*
-	 * @param overlapsWith     it's true if the player is overlaps with an enemy,a iteam , edge or terrains as a type of boolean
+
+	/**
+	 * @param overlapsWith true if the player is overlaps with an enemy, an
+	 * item, an edge, or terrain as a type of boolean.
 	 */
 	public void setoverlapsWith(player player){//set if player is overlapswith enemy , terrain ,potions or edges
 		if (player.xCoord <= Enemy.xCoor + Enemy.xSize && player.xCoord >= Enemy.xCoor - Enemy.xSize && player.yCoord <= Enemy.yCoord + Enemy.ySize && player.yCoord >= Enemy.yCoord - Enemy.ySize){
@@ -123,11 +139,12 @@ public class Sprite
 		}
 		overlapsWith = false;
 		}//it returns false if none of above is true
-		
-	/*
-	 * return overlapsWith      tell if the player is overlap with something or not , as a type of boolean 
+
+	/**
+	 * @return overlapsWith - tell if the player is overlap with something or
+	 * not , as a type of boolean
 	 */
 	public boolean getoverlapsWith(){	//get the overlapsWith
 		return overlapsWith;
-	}		
+	}
 }
