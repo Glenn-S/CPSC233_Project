946860da2bc0b48faf2b9a06f5a11babffb578ea
Glenn <skeltong2@gmail.com>
2018-07-22 13:20:25 -0600

    commit to merge again

--- gameBoard.java
+++ gameBoard.java
@@ -1,5 +1,4 @@
-public class GameBoard
-{
+public class GameBoard {
         /**
        * Purpose: This function is used to create/set up the enemy array ArrayList
        * every enemy has to be instantiated, and set up individually, then added to
