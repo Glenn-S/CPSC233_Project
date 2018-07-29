476941809d1b587d51e2392379d37262c3c0f3ee
momoinori <40900037+momoinori@users.noreply.github.com>
2018-07-29 00:26:36 -0600

    Add Location Test

---
+++ LocationTest.java
@@ -0,0 +1,99 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * @author Nathan Bhandari, Chris Yan, Zachary Udoumoren, Glenn Skelton
+ */
+public class LocationTest {
+
+    @Test
+    public void test_xCoordvalid() {
+    	Location l = new Location() ;
+    	l.setxCoord(20);
+        assertEquals("input a valid xcoord",20,l.getxCoord());
+    }
+
+    
+    @Test
+    public void test_xCoordsmall() {
+    	Location l = new Location() ;
+    	l.setxCoord(1);
+    	l.setxCoord(-1);
+        assertEquals("input a xcoord which is smaller than 0",1,l.getxCoord());
+    }
+    
+    @Test
+    public void test_xCoordlarge() {
+    	Location l = new Location() ;
+    	l.setxCoord(30);
+    	l.setxCoord(84);
+        assertEquals("input a xcoord which is bigger than 80",30,l.getxCoord());
+    }
+    
+    @Test
+    public void test_yCoordvalid() {
+    	Location l = new Location() ;
+    	l.setyCoord(20);
+        assertEquals("input a valid xcoord",20,l.getyCoord());
+    }
+
+    
+    @Test
+    public void test_yCoordsmall() {
+    	Location l = new Location() ;
+    	l.setyCoord(1);
+    	l.setyCoord(-1);
+        assertEquals("input a xcoord which is smaller than 0",1,l.getyCoord());
+    }
+    
+    @Test
+    public void test_yCoordlarge() {
+    	Location l = new Location() ;
+    	l.setyCoord(30);
+    	l.setyCoord(84);
+        assertEquals("input a xcoord which is bigger than 80",30,l.getyCoord());
+    }
+    
+    @Test
+    public void test_Xsizevalid() {
+    	Location l = new Location() ;
+    	l.setxCoord(20);
+        assertEquals("input a valid xcoord",20,l.getxCoord());
+    }
+
+    
+    @Test
+    public void test_xSizesmall() {
+    	Location l = new Location() ;
+    	l.setxCoord(1);
+    	l.setxCoord(-1);
+        assertEquals("input a xcoord which is smaller than 0",1,l.getxCoord());
+    }
+   
+
+    @Test
+    public void test_ySizevalid() {
+    	Location l = new Location() ;
+    	l.setyCoord(20);
+        assertEquals("input a valid xcoord",20,l.getyCoord());
+    }
+
+    
+    @Test
+    public void test_ySizesmall() {
+    	Location l = new Location() ;
+    	l.setyCoord(1);
+    	l.setyCoord(-1);
+        assertEquals("((" + l.getxCoord() + ", " + l.getyCoord() +"), (X Size: " + l.getxSize() + ", Y Size: " + l.getySize() + "))",1,l.getyCoord());
+    }
+   
+    @Test
+    public void test_toString() {
+    	Location l = new Location();
+    	l.setxCoord(20);
+    	l.setyCoord(30);
+    	l.setxSize(100);
+    	l.setySize(50);
+        assertEquals("((20, 30), (X Size: 100, Y Size: 50))",l.toString());
+    }
+}
