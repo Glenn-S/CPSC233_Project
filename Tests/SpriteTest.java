6292128aff01b9b5b79b9fbe1a1499ec8507e162
momoinori <40900037+momoinori@users.noreply.github.com>
2018-07-29 00:25:35 -0600

    Add Sprite Test

---
+++ Tests/SpriteTest.java
@@ -0,0 +1,57 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+/**
+ * @author Nathan Bhandari, Chris Yan, Zachary Udoumoren, Glenn Skelton
+ */
+public class SpriteTest {
+
+    @Test
+    public void test_setName() {
+    	Sprite s = new Sprite();
+    	s.setName("Chris");
+        assertEquals("Chris",s.getName());
+    }
+    
+    @Test
+    public void test_setCoord() {
+    	Sprite s = new Sprite();
+    	Location l = new Location();
+    	l.setxCoord(20);
+    	l.setyCoord(30);
+    	l.setxSize(100);
+    	l.setySize(50);
+    	s.setCoord(l);
+        assertEquals("((20, 30), (X Size: 100, Y Size: 50))",s.getCoord().toString());
+    }
+    
+    @Test
+    public void test_setExistfalse() {
+    	Sprite s = new Sprite();
+    	s.setExists(false);
+        assertEquals(false,s.getExists());
+    }
+    
+    @Test
+    public void test_setExisttrue() {
+    	Sprite s = new Sprite();
+    	s.setExists(true);
+        assertEquals(true,s.getExists());
+    }
+    
+    @Test
+    public void test_toString() {
+    	Sprite s = new Sprite();
+    	Location l = new Location();
+    	s.setName("Eva");
+    	l.setxCoord(20);
+    	l.setyCoord(30);
+    	l.setxSize(100);
+    	l.setySize(50);
+    	s.setCoord(l);
+    	s.setExists(true);
+        assertEquals("Name: Eva, Coord: ((20, 30), (X Size: 100, Y Size: 50)), Image: null, Char:  , Text: null, Exists: true",s.toString());
+    }
+    
+    
+
+}
