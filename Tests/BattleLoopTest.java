c58b4f62070c62dea7de56ceb4c4ab10a46ff577
momoinori <40900037+momoinori@users.noreply.github.com>
2018-08-05 18:06:42 -0600

    Add files via upload

--- Tests/BattleLoopTest.java
+++ Tests/BattleLoopTest.java
@@ -1,7 +1,9 @@
 package Tests;
 
 import static org.junit.Assert.assertEquals;
-
+import Terminal.BattleLoop;
+import Sprite.Enemy;
+import Sprite.Player;
 import org.junit.Test;
 /**
  * @author Nathan Bhandari, Chris Yan, Zachary Udoumoren, Glenn Skelton
@@ -9,8 +11,62 @@ import org.junit.Test;
 public class BattleLoopTest {
 
 	@Test
-	public void test() {
-	assertEquals();
+	public void testEuseParry() {
+		BattleLoop b = new BattleLoop();
+		b.setEnemyUsedParry(true);
+	assertEquals(true,b.getEnemyUsedParry());
 	}
 	
+	@Test
+	public void testENotuseParry() {
+		BattleLoop b = new BattleLoop();
+		b.setEnemyUsedParry(false);
+	assertEquals(false,b.getEnemyUsedParry());
+	}
+	
+	@Test
+	public void testPuseParry() {
+		BattleLoop b = new BattleLoop();
+		b.setUsedParry(true);
+	assertEquals(true,b.getUsedParry());
+	}
+	
+	@Test
+	public void testPNotuseParry() {
+		BattleLoop b = new BattleLoop();
+		b.setUsedParry(false);
+	assertEquals(false,b.getUsedParry());
+	}
+	
+	@Test
+	public void testWinstateF() {
+		BattleLoop b = new BattleLoop();
+		Enemy e = new Enemy();
+		e.setHealth(20);
+	assertEquals(false,b.checkWinState(e));
+	}
+	
+	@Test
+	public void testWinstateT() {
+		BattleLoop b = new BattleLoop();
+		Enemy e = new Enemy();
+		e.setHealth(0);
+	assertEquals(true,b.checkWinState(e));
+	}
+	
+	@Test
+	public void testLoseStateF() {
+		BattleLoop b = new BattleLoop();
+		Player p = new Player();
+		p.setHealth(20);
+	assertEquals(false,b.checkLoseState(p));
+	}
+	
+	@Test
+	public void testLoseStateT() {
+		BattleLoop b = new BattleLoop();
+		Player p = new Player();
+		p.setHealth(0);
+	assertEquals(true,b.checkLoseState(p));
+	}
 }
