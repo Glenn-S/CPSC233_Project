d7aa8f667b991b67a1c641c15866979ff2ff8cc0
Inori <Yuki@Inori.local>
2018-08-11 00:03:53 -0600

    tests

--- Tests/SpriteTest.java
+++ Tests/SpriteTest.java
@@ -15,9 +15,9 @@ public class SpriteTest {
     public void test_setName() {
     	Sprite s = new Sprite();
     	s.setName("Chris");
-        assertEquals("Chris",s.getName());
+      assertEquals("Chris",s.getName());
     }
-    
+
     @Test
     public void test_setCoord() {
     	Sprite s = new Sprite();
@@ -27,23 +27,23 @@ public class SpriteTest {
     	l.setxSize(100);
     	l.setySize(50);
     	s.setCoord(l);
-        assertEquals("((20, 30), (X Size: 100, Y Size: 50))",s.getCoord().toString());
+      assertEquals("((20, 30), (X Size: 100, Y Size: 50))",s.getCoord().toString());
     }
-    
+
     @Test
     public void test_setExistfalse() {
     	Sprite s = new Sprite();
     	s.setExists(false);
-        assertEquals(false,s.getExists());
+      assertEquals(false,s.getExists());
     }
-    
+
     @Test
     public void test_setExisttrue() {
     	Sprite s = new Sprite();
     	s.setExists(true);
-        assertEquals(true,s.getExists());
+      assertEquals(true,s.getExists());
     }
-    
+
     @Test
     public void test_toString() {
     	Sprite s = new Sprite();
@@ -55,9 +55,6 @@ public class SpriteTest {
     	l.setySize(50);
     	s.setCoord(l);
     	s.setExists(true);
-        assertEquals("Name: Eva, Coord: ((20, 30), (X Size: 100, Y Size: 50)), Image: null, Char:  , Text: null, Exists: true",s.toString());
+      assertEquals("Name: Eva, Coord: ((20, 30), (X Size: 100, Y Size: 50)), Image: null, Char:  , Text: null, Exists: true",s.toString());
     }
-    
-    
-
 }
